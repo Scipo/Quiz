dc7cd808fcc9a81dd83f677f749762a9a95a584a
Scipo <ivots@mail.bg>
2019-03-04 01:01:29 +0200

    Android app for learning

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.quiz;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
